7068c476f300ba3154de923b39f161749e5d3131
Am1n <kido@DESKTOP-0TC54R2>
2024-08-05 00:18:20 +0330

    All Tasks are Done, fix the last task

--- streamApi/src/Error.java
+++ streamApi/src/Error.java
@@ -1,11 +1,17 @@
 public class Error {
 
     public int errorNumber;
-    public String errorDetail;
+    public String errorDetail = "";
 
-    public Error(int x, String str){
+    public Error(int x, String[] str){
         errorNumber = x;
-        errorDetail = str;
+        for (int i = 1; i < str.length; i++) {
+            errorDetail += str[i];
+        }
+    }
+
+    public String getErrorDetail(){
+        return errorDetail;
     }
 
 
--- streamApi/src/Main.java
+++ streamApi/src/Main.java
@@ -2,10 +2,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -28,13 +25,15 @@ public class Main {
         List<Error> data = new ArrayList<>();
 
         while ((result = br.readLine())!= null){
-            data.add(new Error(Integer.parseInt(result.split(" ")[0]), result.split(" ")[1]));
+            data.add(new Error(Integer.parseInt(result.split(" ")[0]), result.split(" ")));
         }
 
         System.out.println("\n\nList of all responses:");
         List<String> task1 = data.stream()
                 .distinct()
-                .map( n -> n.errorDetail).collect(Collectors.toList());
+                .map( n -> n.errorDetail)
+                .distinct()
+                .collect(Collectors.toList());
 
         System.out.println(task1);
 
@@ -69,49 +68,34 @@ public class Main {
 
 
 
-        
+
 
 
 
         double all = itemCount.values().stream().mapToInt(Long::intValue).sum();
         double tedad = itemCount.values().size();
 
-        double Avg = all/tedad;
+        double avg = all/tedad;
 
-        System.out.println("\n\n\nThe Average count for all errors is: " + Avg);
+        System.out.println("\n\n\nThe Average count for all errors is: " + avg);
 
         System.out.println("\nList of all errors above avg:");
 
         List <Integer> task5 = itemCount.entrySet()
-                .stream().
-                map( n -> n.toString())
-                .filter( (String n) -> Integer.parseInt(n.split("=")[1]) > Avg)
-                .map( n -> Integer.parseInt(n.split("=")[0]))
+                .stream()
+                .filter(entry -> entry.getValue() > avg)
+                .map(Map.Entry::getKey)
                 .collect(Collectors.toList());
 
         System.out.println(task5);
 
 
-        System.out.println("\n\n\nErrors in range of 400:");
-        List<String> task400Range = data.stream()
-                .filter(n -> n.errorNumber > 399 && n.errorNumber < 500)
-                .map( n -> n.errorDetail)
-                .collect(Collectors.toList());
+        System.out.println("\n\n\nErrors based on the range:");
 
-        System.out.println(task400Range);
+        Map<Object, Set<String>> groupedByRange = data.stream().distinct()
+                .collect(Collectors.groupingBy(e -> (e.errorNumber/100) *100, Collectors.mapping(Error::getErrorDetail, Collectors.toSet())));
 
-
-
-        System.out.println("\n\n\nErrors in range of 500:");
-        List<String> task500Range = data.stream()
-                .filter(n -> n.errorNumber > 499 && n.errorNumber < 600)
-                .map( n -> n.errorDetail)
-                .collect(Collectors.toList());
-
-        System.out.println(task500Range);
-
-
-
+        groupedByRange.entrySet().stream().distinct().forEach( s -> System.out.println(s));
 
 
 
